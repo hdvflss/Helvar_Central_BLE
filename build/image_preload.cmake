1df03b09fbbf07ef6d4261ff8ac29b2f93962b87
Huy Vu <hdvflss@gmail.comm>
2024-08-29 14:23:14 +0300

    Initial Commit

---
+++ build/image_preload.cmake
@@ -0,0 +1,7 @@
+# Generated file that can be used to preload variant images
+set(BOARD "ubx_bmd300eval_nrf52832" CACHE INTERNAL "NCS child image controlled")
+set(CACHED_CONF_FILE "c:/Users/a/Desktop/Aalto/ELEC-D0301_Protocamp/Helvar_Central_BLE/prj.conf" CACHE INTERNAL "NCS child image controlled")
+set(CONF_FILE_BUILD_TYPE "" CACHE INTERNAL "NCS child image controlled")
+set(DTC_OVERLAY_FILE "" CACHE INTERNAL "NCS child image controlled")
+set(NCS_TOOLCHAIN_VERSION "NONE" CACHE INTERNAL "NCS child image controlled")
+set(WEST_PYTHON "C:/ncs/toolchains/cf2149caf2/opt/bin/python.exe" CACHE INTERNAL "NCS child image controlled")
